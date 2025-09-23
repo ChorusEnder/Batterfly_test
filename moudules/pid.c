a2514351e50c18f1a4a3ba6ca6804b3be02442bb
endchoru <2659925034@qq.com>
2025-09-23 20:46:22 +0800

    新增pid模块

---
+++ moudules/pid.c
@@ -0,0 +1,25 @@
+#include "pid.h"
+
+static void OutputLimit(PID_Instance_s *pid)
+{
+    if (pid->output > pid->maxout) {
+        pid->output = pid->maxout;
+    } else if (pid->output < -pid->maxout) {
+        pid->output = -pid->maxout;
+    }
+}
+
+float PIDCalculate(PID_Instance_s *pid, float measure, float target)
+{
+    //更新误差
+    pid->err[2] = pid->err[1];
+    pid->err[1] = pid->err[0];
+    pid->err[0] = target - measure;
+
+    //计算PID,采用增量式PID
+    float output = pid->kp * pid->err[0] - pid->ki * pid->err[1] + pid->kd * pid->err[2];
+
+    OutputLimit(pid);
+    
+    return output;
+}
---
+++ moudules/pid.h
@@ -0,0 +1,20 @@
+#ifndef PID_H
+#define PID_H
+
+#include "main.h"
+
+typedef struct {
+    float kp;       // 比例系数
+    float ki;       // 积分系数
+    float kd;       // 微分系数
+
+    float ref;      // 目标值
+    float err[3];   // 误差数组
+
+    float output;   // 输出值
+    float maxout; // 输出限幅
+} PID_Instance_s;
+
+float PIDCalculate(PID_Instance_s *pid, float measure, float target);
+
+#endif

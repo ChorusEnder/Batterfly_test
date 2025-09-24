dd4cae33082a9bdc4270980b964667e33f4835d6
endchoru <2659925034@qq.com>
2025-09-24 22:32:51 +0800

    freertos

--- Core/Src/tim.c
+++ Core/Src/tim.c
@@ -42,7 +42,6 @@ void MX_TIM3_Init(void)
 
   /* USER CODE END TIM3_Init 1 */
   htim3.Instance = TIM3;
-  // htim3.Init.Prescaler = 20-1;
   htim3.Init.Prescaler = 20-1;
   htim3.Init.CounterMode = TIM_COUNTERMODE_UP;
   htim3.Init.Period = 100-1;

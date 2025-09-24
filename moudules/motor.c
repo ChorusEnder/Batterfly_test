cfec8ba89ea0c3d96d8cd2289676e32826177f71
endchoru <2659925034@qq.com>
2025-09-24 21:48:42 +0800

    新增电机模块

---
+++ moudules/motor.c
@@ -0,0 +1,81 @@
+
+#include "motor.h"
+
+//模块'motor'的私有变量,用于保存电机实例的地址
+static Motor_Instance_s *motor_instance[MOTOR_COUNT] = {0};
+
+/**
+ * @brief 电机初始化函数,由于一个电机需要两个PWM信号操作,后面考虑采用电机实例
+ * 
+ */
+Motor_Instance_s* Motor_Init(Motor_Init_Config_s *config)
+{
+    Motor_Instance_s *instance = (Motor_Instance_s *)malloc(sizeof(Motor_Instance_s));
+    memset(instance, 0, sizeof(Motor_Instance_s));
+
+    instance->pwm_config = config->pwm_config;
+    instance->controller.angle_pid = config->controller.angle_pid;
+    instance->setting = config->setting;
+
+    return instance;
+}
+
+/**
+ * @brief 电机驱动函数,根据输入值控制电机正反转和速度
+ * @param value 范围为正负比较寄存器的值,
+ */
+void MotorDrive(int16_t value, Motor_PWM_Config_s *pwm_config)
+{
+    if(value > VALUE_COMPARE) value = VALUE_COMPARE;
+    if(value < -VALUE_COMPARE) value = -VALUE_COMPARE;
+
+    if(value >= 0)
+    {
+        __HAL_TIM_SET_COMPARE(pwm_config->htim, pwm_config->channel1, value);
+        __HAL_TIM_SET_COMPARE(pwm_config->htim, pwm_config->channel2, 0);
+    }
+    else
+    {
+        __HAL_TIM_SET_COMPARE(pwm_config->htim, pwm_config->channel1, 0);
+        __HAL_TIM_SET_COMPARE(pwm_config->htim, pwm_config->channel2, -value);
+    }
+}
+
+
+/**
+ * @brief 由于用两个PWM控制一个电机,所以这里的速度是-100~100,根据正负号改变PWM信号使能,从而改变方向
+ * @brief 目前只有角度单环控制,后面考虑加速度环
+ * @param speed 范围-100~100
+ */
+void MotorControl()
+{
+    Motor_Instance_s *motor;
+    Motor_PWM_Config_s *pwm_config;
+    Motor_Setting_s *setting;
+    Motor_Controller_s *controller;
+    Motor_Measures_s *measures;
+    float pid_ref;
+    float measure;
+
+    for(int i = 0; i < MOTOR_COUNT; i++)
+    {
+        if(motor_instance[i] == NULL) break;
+        motor = motor_instance[i];
+        setting = &motor->setting;
+        controller = &motor->controller;
+        measures = &motor->measures;
+        pwm_config = &motor->pwm_config;
+
+        //角度环计算
+        measure = measures->angle;
+        pid_ref = controller->pid_ref;
+        pid_ref = PIDCalculate(&controller->angle_pid, measure, pid_ref);
+
+        if (setting->reverse == MOTOR_DIR_REVERSE) {
+            pid_ref = -pid_ref;
+        }
+
+        MotorDrive((int16_t)pid_ref, pwm_config);
+
+    }
+}
---
+++ moudules/motor.h
@@ -0,0 +1,58 @@
+#ifndef MOTOR_H
+#define MOTOR_H
+
+#include "main.h"
+#include "pid.h"
+
+//比较寄存器的值,根据实际设置更改
+#define VALUE_COMPARE 1000
+#define MOTOR_COUNT 4
+
+extern TIM_HandleTypeDef htim3;
+
+typedef enum {
+    MOTOR_DIR_NORMAL = 0,
+    MOTOR_DIR_REVERSE = 1,
+} Motor_Reverse_Flag_e;
+
+//电机设置
+typedef struct {
+    Motor_Reverse_Flag_e reverse; // 反转标志
+} Motor_Setting_s;
+
+typedef struct {
+    TIM_HandleTypeDef *htim;
+    uint32_t channel1;
+    uint32_t channel2;
+} Motor_PWM_Config_s;
+
+
+typedef struct {
+    float angle;
+    float speed;
+} Motor_Measures_s;
+
+//电机控制器
+typedef struct {
+    float pid_ref;
+    PID_Instance_s angle_pid;
+} Motor_Controller_s;
+
+typedef struct {
+    Motor_PWM_Config_s pwm_config;  //对应的时钟句柄指针和通道
+    Motor_Setting_s setting;
+    Motor_Measures_s measures;
+    Motor_Controller_s controller;
+} Motor_Instance_s;
+
+typedef struct {
+    Motor_PWM_Config_s pwm_config;
+    Motor_Controller_s controller;
+    Motor_Setting_s setting;
+} Motor_Init_Config_s;
+
+
+Motor_Instance_s* Motor_Init(Motor_Init_Config_s *config);
+void MotorControl();
+
+#endif
